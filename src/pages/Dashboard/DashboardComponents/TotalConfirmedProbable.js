695054e1ac8a4cd5c0d5bdd4001c407b87eed23e
Hun Lam <hun.lam97@gmail.com>
2021-01-23 15:36:41 +0700

    feat: bruh

---
+++ src/pages/Dashboard/DashboardComponents/TotalConfirmedProbable.js
@@ -0,0 +1,3 @@
+export const TotalConfirmedProbable = () => {
+  const [data, setData] = useState();
+};
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,10 +1,14 @@
-import React from "react";
-import ReactFC from "react-fusioncharts";
-import FusionCharts from "fusioncharts";
-import Charts from "fusioncharts/fusioncharts.charts";
-import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
-// Resolves charts dependancy
-ReactFC.fcRoot(FusionCharts, Charts, FusionTheme);
+import React, { useState, useEffect, useMemo } from "react";
+import View from "../components/View";
+import { ViewChart } from "../components/ViewChart";
+// import { connect } from 'react-redux'
+
+// const dataConfig = (chartType,config) => {
+//     switch(chartType):
+//     case:
+// }
+
+const chartType = { stack: "scrollstackedcolumn2d", single: "scrollColumn2d" };
 
 const dataSource = {
   chart: {
@@ -547,14 +551,75 @@ const dataSource = {
   ],
 };
 
-const chartConfig = {
-  type: "scrollstackedcolumn2d",
-  width: "100%",
-  height: "100%",
-  dataFormat: "json",
-  dataSource,
-};
+export const Dashboard = () => {
+  const [fakeData] = useState([
+    {
+      title: "Total Suspected Cases - Current year",
+      description: "COVID-19 Suspected cases",
+      organization: "Test Organization",
+      period: "Months this year",
+      result: 5,
+    },
+    {
+      title: "Laboratory confirmed cases - Current year",
+      description: "COVID-19 Laboratory confirmed cases - by onset of symptoms",
+      organization: "Test Organization",
+      period: "This year",
+      result: "",
+    },
+    {
+      title: "Total deaths (among suspected cases)",
+      description: "COVID-19 Deaths (all suspected cases)",
+      organization: "Test Organization",
+      period: "This year",
+      result: "",
+    },
+    {
+      title: "Total recovered (among confirmed cases)",
+      description: "COVID-19 Recovered confirmed cases",
+      organization: "Test Organization",
+      period: "This year",
+      result: "",
+    },
+  ]);
 
-export const ViewChart = (props) => {
-  return <ReactFC {...props.chartConfig} />;
+  const [fakeChartData] = useState(dataSource);
+
+  const chartConfig = useMemo(() => {
+    const defaultConfig = {
+      type:
+        fakeChartData.dataset.length > 1 ? chartType.stack : chartType.single,
+      width: "100%",
+      height: "100%",
+      dataFormat: "json",
+      dataSource,
+    };
+    return defaultConfig;
+  }, [fakeChartData]);
+
+  return (
+    <>
+      <div className="dashboard-wrapper">
+        <div className="header">
+          <h1>DHIS 2 - Dashboard</h1>
+        </div>
+        {/* <SearchBar /> */}
+        <div className="titlebar-wrapper">
+          <h2>01. COVID-19 Surveillance (Tracker)</h2>
+        </div>
+        <div className="grid-wrapper">
+          <div className="row">
+            <View data={fakeData[0]} />
+            <View data={fakeData[1]} />
+            <View data={fakeData[2]} />
+            <View data={fakeData[3]} />
+          </div>
+          <div>
+            <ViewChart chartConfig={chartConfig} />
+            {/* <ViewChart data={chartConfig} /> */}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
